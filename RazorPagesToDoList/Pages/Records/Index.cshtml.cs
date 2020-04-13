651224266f796155948a9a4ec1d4a0bb8269b73c
evgeniy.shelukhin <evgeniy.shelukhin@emotorwerks.com>
2020-04-13 14:04:42 +0300

    A: exporting as excel

--- RazorPagesToDoList/Pages/Records/Index.cshtml.cs
+++ RazorPagesToDoList/Pages/Records/Index.cshtml.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesToDoList.Models;
+using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
+using System.IO;
 
 namespace RazorPagesToDoList.Pages.Records
 {
@@ -24,36 +27,52 @@ namespace RazorPagesToDoList.Pages.Records
 
         public IList<Record> Record { get;set; }
 
-        public async Task OnGetAsync(string sortOrder)
+        public async void OnGetAsync(string sortOrder)
         {
+
             TitleSort = String.IsNullOrEmpty(sortOrder) ? "title_desc" : "";
             CreatedDateSort = String.IsNullOrEmpty(sortOrder) ? "createdDate_desc" : "";
             EditedDateSort = String.IsNullOrEmpty(sortOrder) ? "editedDate_desc" : "";
             IsDoneSort = String.IsNullOrEmpty(sortOrder) ? "isDone_desc" : "";
+            Record = await GetSortedRecordsIQ(sortOrder).AsNoTracking().ToListAsync();
+        }
 
+        private IQueryable<Record> GetSortedRecordsIQ(string sortOrder)
+        {
             IQueryable<Record> recordsIQ = from r in _context.Record
-                                            select r;
+                                           select r;
 
-            switch (sortOrder)
+            recordsIQ = sortOrder switch
             {
-                case "title_desc":
-                    recordsIQ = recordsIQ.OrderByDescending(r => r.Title);
-                    break;
-                case "createdDate_desc":
-                    recordsIQ = recordsIQ.OrderByDescending(r => r.CreatedDate);
-                    break;
-                case "editedDate_desc":
-                    recordsIQ = recordsIQ.OrderByDescending(r => r.EditedDate);
-                    break;
-                case "isDone_desc":
-                    recordsIQ = recordsIQ.OrderByDescending(r => r.IsDone);
-                    break;
-                default:
-                    recordsIQ = recordsIQ.OrderBy(r => r.Title);
-                    break;
+                "title_desc" => recordsIQ.OrderByDescending(r => r.Title),
+                "createdDate_desc" => recordsIQ.OrderByDescending(r => r.CreatedDate),
+                "editedDate_desc" => recordsIQ.OrderByDescending(r => r.EditedDate),
+                "isDone_desc" => recordsIQ.OrderByDescending(r => r.IsDone),
+                _ => recordsIQ.OrderBy(r => r.Title),
+            };
+            return recordsIQ;
+        }
+
+        public async Task<IActionResult> OnPostExportAsExcelAsync()
+        {
+            var stream = new MemoryStream();
+            var records = await _context.Record.AsNoTracking().ToListAsync();
+
+            using (var package = new ExcelPackage(stream))
+            {
+                var workSheet = package.Workbook.Worksheets.Add("Sheet1");
+                string dateformat = "m/d/yy h:mm:ss";
+                workSheet.Cells.LoadFromCollection(records, true);
+                workSheet.Column(3).Style.Numberformat.Format = dateformat;
+                workSheet.Column(4).Style.Numberformat.Format = dateformat;
+                package.Save();
             }
 
-            Record = await recordsIQ.AsNoTracking().ToListAsync();
+            stream.Position = 0;
+
+            string fileName = $"Records-{DateTime.Now:yyyyMMddHHmmssfff}.xlsx";
+            return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
         }
+
     }
 }
